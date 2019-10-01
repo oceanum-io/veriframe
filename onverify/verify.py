4f99fa22e07328485223b93972a3f5c22575a994
rafa-guedes <ocerafa@gmail.com>
2019-10-01 17:59:22 -0300

    Renaming verify module

--- onverify/verify.py
+++ onverify/verify.py
@@ -1,3 +1,13 @@
-# -*- coding: utf-8 -*-
+"""Verify object."""
 
-"""Main module."""
+import pandas as pd
+
+
+class Verify:
+    """Verify dataframe representing collocated pairs."""
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "<Verify>"
