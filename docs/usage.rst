ae99fe861263d07112606224dc9933bb2d23f6e9
rafa-guedes <ocerafa@gmail.com>
2019-09-30 21:00:41 -0300

    Added files for initial project

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use onverify in a project::
+
+    import onverify
---
+++ onverify/cli.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for onverify."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for onverify."""
+    click.echo("Replace this message by putting your code into "
+               "onverify.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
