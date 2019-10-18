7f417a70216fe78f94256704698d4338d1b26bf2
rafa-guedes <ocerafa@gmail.com>
2019-10-18 13:13:20 -0300

    Renaming arguments

--- onverify/stats.py
+++ onverify/stats.py
@@ -2,80 +2,80 @@
 import numpy as np
 
 
-def mad(xarr, yarr, norm=False):
+def mad(x, y, norm=False):
     """Mean absolute difference MAD.
 
     :math:`MAD = \\frac{1}{N}{\\sum_{i=1}^N {\\left|A_i-B_i \\right|}}}`
 
     Args:
-        xarr (array): x values, usually observations.
-        yarr (array): y values, usually model.
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
         norm (bool): Normalise MAD by xmean.
 
     """
-    ret = np.mean(np.abs(yarr - xarr))
+    ret = np.mean(np.abs(y - x))
     if norm:
-        ret /= np.mean(xarr)
+        ret /= np.mean(x)
     return ret
 
 
-def rmsd(xarr, yarr, norm=False):
+def rmsd(x, y, norm=False):
     """Root-mean-square difference.
 
     :math:`RMSD = \\sqrt{\\frac{1}{N}{\\sum_{i=1}^N {\\left(A_i-B_i \\right)^2}}}`
 
     Args:
-        xarr (array): x values, usually observations.
-        yarr (array): y values, usually model.
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
         norm (bool): Normalise MAD by xmean.
 
     """
-    ret = np.mean(np.sqrt((yarr - xarr) ** 2))
+    ret = np.mean(np.sqrt((y - x) ** 2))
     if norm:
-        ret /= np.mean(xarr)
+        ret /= np.mean(x)
     return ret
 
 
-def bias(xarr, yarr, norm=False):
+def bias(x, y, norm=False):
     """Bias.
 
     :math:`Bias = {\\frac 1 N}{\\sum_{i=1}^N {A_i-B_i}}`
 
     Args:
-        xarr (array): x values, usually observations.
-        yarr (array): y values, usually model.
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
         norm (bool): Normalise MAD by xmean.
 
     """
-    ret = np.mean(yarr - xarr)
+    ret = np.mean(y - x)
     if norm:
-        ret /= np.mean(xarr)
+        ret /= np.mean(x)
     return ret
 
 
-def si(xarr, yarr):
+def si(x, y):
     """Scatter Index.
 
     :math:`SI = {\\frac { \\sqrt { {\\frac 1 N} { \\sum_{i=1}^N {\\left(\\left(A_i-{\\overline A}\\right)-\\left(B_i-{\\overline B}\\right)\\right)^2}}} }{  \overline B} }`
 
     Args:
-        xarr (array): x values, usually observations.
-        yarr (array): y values, usually model.
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
 
     """
-    diff_values = yarr - xarr
-    bias_values = bias(xarr, yarr)
-    return np.sqrt(np.mean((diff - bias) ** 2)) / np.mean(xarr)
+    diff_values = y - x
+    bias_values = bias(x, y)
+    return np.sqrt(np.mean((diff - bias) ** 2)) / np.mean(x)
 
 
-def r(xarr, yarr):
+def r(x, y):
     """Pearson Correlation Coeficient.
 
     :math:`R = ...`
  
     Args:
-        xarr (array): x values, usually observations.
-        yarr (array): y values, usually model.
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
 
     """
-    return np.corrcoef(yarr, xarr)[0, 1]
+    return np.corrcoef(y, x)[0, 1]
