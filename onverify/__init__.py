dfc482f72e2fdb2a7913b5f4a6096976cb7575b7
rafa-guedes <ocerafa@gmail.com>
2019-10-18 22:09:15 -0300

    Define some globals

--- onverify/__init__.py
+++ onverify/__init__.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for onverify."""
+import os
+import yaml
+from attrdict import AttrDict
+
 
 __author__ = """Oceanum Developers"""
 __email__ = "developers@oceanum.science"
 __version__ = "0.1.0"
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(HERE, "vardef.yml")) as stream:
+    VARDEF = AttrDict(yaml.load(stream, yaml.SafeLoader))
+
+with open(os.path.join(HERE, "defaults.yml")) as stream:
+    DEFAULTS = AttrDict(yaml.load(stream, yaml.SafeLoader))
