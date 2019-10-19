e80f2917b1710a94442e5f34fa53db0397bb1896
rafa-guedes <ocerafa@gmail.com>
2019-10-18 21:05:48 -0300

    Add a couple of stats functions

--- onverify/stats.py
+++ onverify/stats.py
@@ -1,6 +1,6 @@
 """Stats functions."""
 import numpy as np
-
+from scipy.stats import ks_2samp
 
 def mad(x, y, norm=False):
     """Mean absolute difference MAD.
@@ -17,6 +17,20 @@ def mad(x, y, norm=False):
     if norm:
         ret /= np.mean(x)
     return ret
+
+
+def mrad(x, y, **kwargs):
+    """Mean Relative Absolute Deviation MRAD.
+
+    :math:`MRAD = {\\frac 1 N}{\\sum_{i=1}^N {|\\frac {A_i-B_i} {B_i}|}}`
+
+    Args:
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
+
+    """
+    xmask = np.ma.masked_values(x, 0.)
+    return np.mean(np.abs((y - x) / xmask))
 
 
 def rmsd(x, y, norm=False):
@@ -65,7 +79,7 @@ def si(x, y):
     """
     diff_values = y - x
     bias_values = bias(x, y)
-    return np.sqrt(np.mean((diff - bias) ** 2)) / np.mean(x)
+    return np.sqrt(np.mean((diff_values - bias_values) ** 2)) / np.mean(x)
 
 
 def r(x, y):
@@ -79,3 +93,16 @@ def r(x, y):
 
     """
     return np.corrcoef(y, x)[0, 1]
+
+
+def ks(x, y):
+    """Kolmogorov-Smirnov statistic.
+
+    :math:`D = {\\max(|F1(x)-F2(x)|)}`
+
+    Args:
+        x (array): x values, usually observations.
+        y (array): y values, usually model.
+
+    """
+    return ks_2samp(x, y)[0]
